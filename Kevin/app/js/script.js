347dcef15df5a8e4984eb524d12d8f6502ba4aac
Kevin Kagubari <kagubarikevin@gmail.com>
2022-02-09 14:15:48 +0100

    Finished hamburger navbar fr

--- Kevin/app/js/script.js
+++ Kevin/app/js/script.js
@@ -6,10 +6,18 @@ const navMenu = document.querySelector(".nav-menu")
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active"); 
     navMenu.classList.toggle("active"); 
+    }   
+)
+document.querySelectorAll(".nav-link").forEach(n => 
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active")
+        navMenu.classList.remove("active")
+        }
+    )
+)
 
-})
 
-window.addEventListener("scroll", () => {
-    const headr = document.querySelector("header")
-    headr.classList.toggle("sticky", window.scrollY > 0)
-})
+// window.addEventListener("scroll", () => {
+//     const headr = document.querySelector("header")
+//     headr.classList.toggle("sticky", window.scrollY > 0)
+// })
