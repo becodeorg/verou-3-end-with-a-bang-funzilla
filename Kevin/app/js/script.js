b98840172506b286b1aa02d6ccdd8878d4f6a217
Kevin Kagubari <kagubarikevin@gmail.com>
2022-02-09 13:57:44 +0100

    Finished hamburger navbar

--- Kevin/app/js/script.js
+++ Kevin/app/js/script.js
@@ -1,1 +1,15 @@
-console.log('Test 1,2 and 3')
+console.log('Test 1,5 and 3')
+
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active"); 
+    navMenu.classList.toggle("active"); 
+
+})
+
+window.addEventListener("scroll", () => {
+    const headr = document.querySelector("header")
+    headr.classList.toggle("sticky", window.scrollY > 0)
+})
