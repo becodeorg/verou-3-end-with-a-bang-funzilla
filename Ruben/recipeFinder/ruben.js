e400123c449ff8da283595047ded095fc3a9769b
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-02-10 12:59:15 +0100

    Bugfix in missingIngrList, if path doesn't exist

---
+++ Ruben/recipeFinder/ruben.js
@@ -0,0 +1,71 @@
+import Data from "../../config.js";
+
+const ingredientsList = document.getElementById("ingredientsList");
+const addBtn = document.getElementById("addBtn")
+const searchBtn = document.getElementById("searchBtn");
+let ingredientsArray = [];
+
+
+
+addBtn.addEventListener("click", () => {
+    const searchBar = document.getElementById("searchBar");
+    const searchInput = searchBar.value.toLowerCase();
+    const newListItem = document.createElement("li");
+    newListItem.innerHTML = searchInput;
+    ingredientsList.appendChild(newListItem)
+
+    ingredientsArray.push(searchInput)
+
+    console.log(ingredientsArray);
+})
+
+const createElement = (element, className, parent) => {
+    const newElement = document.createElement(element);
+    newElement.classList = className;
+    parent.appendChild(newElement);
+    return newElement;
+};
+
+searchBtn.addEventListener("click", () => {
+    const recipeWrapper = document.getElementById("displayRecipes")
+    recipeWrapper.innerHTML = "";
+    ingredientsList.innerHTML = "";
+    let joinedIngredients = ingredientsArray.join();
+    console.log(joinedIngredients);
+
+    fetch("https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + Data.SPOONACULAR_API + "&number=20&ignorePantry=true&ranking=2&ingredients=" + joinedIngredients)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                const recipeWrapper = document.getElementById("displayRecipes")
+                const recipeLink = createElement("a", "recipeLink", recipeWrapper);
+                recipeLink.href = "https://spoonacular.com/recipes/-" + data[i].id
+                recipeLink.target = "_blank"
+                const card = createElement("div", "card", recipeLink);
+                const topPart = createElement("img", "topPart", card);
+                topPart.src = data[i].image
+                const infoPart = createElement("div", "infoPart", card);
+                const recipeTitle = createElement("h3", "recipeTitle", infoPart);
+                recipeTitle.innerHTML = data[i].title
+                if (typeof data[0].missedIngredients[0] != "undefined") {
+                    const missingIngrDiv = createElement("div", "missingIngrDiv", infoPart);
+                    const missingIngrTitle = createElement("h5", "missingIngrTitle", missingIngrDiv);
+                    missingIngrTitle.innerHTML = "Missing Ingredients: "
+                    const missingIngrList = createElement("ul", "missingIngrList", missingIngrDiv);
+                    for (let x = 0; x < 2; x++) {
+                        if (typeof data[i].missedIngredients[x] != "undefined") {
+                            const missingListItem = createElement("li", "missingListItem", missingIngrList);
+                            missingListItem.innerHTML = data[i].missedIngredients[x].name;
+                        } else {
+                            return
+                        }
+                    }
+                    if (typeof data[3].missedIngredients[3] != "undefined") {
+                        const moreMissingItems = createElement("li", "moreMissingItems", missingIngrList);
+                        moreMissingItems.innerHTML = "& More, see recipe."
+                    }
+                }
+            }
+        })
+})
